efbfc6ef7e1a61c4fee9ab0ad03a009527de6140
hatchikuji <swannbrillant@gmail.com>
2024-11-13 20:58:12 +0100

    Mise en forme

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
     var dropdown = document.querySelector('.dropdown');
     var dropdownContent = document.querySelector('.dropdown-content');
+    var dropbtn = document.querySelector('.dropbtn');
 
-    dropdown.addEventListener('mouseover', function() {
-        dropdownContent.style.display = 'block';
-    });
+    function isMobileDevice() {
+        return /Mobi|Android/i.test(navigator.userAgent);
+    }
 
-    dropdown.addEventListener('mouseout', function() {
-        dropdownContent.style.display = 'none';
-    });
+    if (isMobileDevice()) {
+        dropbtn.addEventListener('click', function(event) {
+            event.preventDefault();
+            if (dropdownContent.style.display === 'block') {
+                dropdownContent.style.display = 'none';
+            } else {
+                dropdownContent.style.display = 'block';
+            }
+        });
+    } else {
+        dropdown.addEventListener('mouseover', function() {
+            dropdownContent.style.display = 'block';
+        });
+
+        dropdown.addEventListener('mouseout', function() {
+            dropdownContent.style.display = 'none';
+        });
+    }
 });
